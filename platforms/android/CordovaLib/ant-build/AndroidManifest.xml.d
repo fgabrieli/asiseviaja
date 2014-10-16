671c5337e002e443b28917e2590327afd54b42f0
fgabrieli@gmail.com <fgabrieli@gmail.com>
2014-10-16 12:51:06 -0300

    Deleting local env directories, no need to have them in the repo

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\home\asiseviaja\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\home\asiseviaja\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\home\asiseviaja\platforms\android\ant-build\AndroidManifest.xml : \
-C:\home\asiseviaja\platforms\android\AndroidManifest.xml \
