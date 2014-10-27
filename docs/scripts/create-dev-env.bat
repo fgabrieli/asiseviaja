bf6493b8c4736b96cc5f29648967e7ea577a457a
fgabrieli@gmail.com <fgabrieli@gmail.com>
2014-10-27 16:29:17 -0300

    Adding docs to configure the environment.

---
+++ docs/scripts/create-dev-env.bat
@@ -0,0 +1,8 @@
+call cordova platform add android
+call cordova plugins add org.apache.cordova.camera
+call cordova plugins add org.apache.cordova.file
+call cordova plugins add org.apache.cordova.file-transfer
+call cordova plugins add org.apache.cordova.inappbrowser
+call cordova plugins add org.apache.cordova.media
+call cordova plugins add org.apache.cordova.media-capture
+call cordova plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git
