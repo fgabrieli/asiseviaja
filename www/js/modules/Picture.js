95e1be6f9d0dd62c41f872cd58a6a52ecc5021d6
Fernando Gabrieli <fgabrieli@gmail.com>
2014-11-02 20:21:01 -0300

    Removed service calls after refactoring.

--- www/js/modules/Picture.js
+++ www/js/modules/Picture.js
@@ -1,35 +1,6 @@
 asi.Picture = {
   init : function() {
-    var t = asi.Picture;
-
-    Event.bind(asi.evt.takePicture, 'PictureModule', function() {
-      t.takePicture();
-    });
-    
-    
-  },
-  takePicture : function() {
-    asiLog('takePicture');
-
-    var onSuccess = function(imageUri) {
-      Event.fire(asi.evt.pictureTaken, {
-        imageUri : imageUri
-      });
-    };
-
-    var onFail = function(message) {
-      console.log("$scope.takePicture() failed, error message: ", message);
-    };
-
-    asiLog('navigator.camera.getPicture()');
-
-    navigator.camera.getPicture(onSuccess, onFail, {
-      quality : 50,
-      destinationType : Camera.DestinationType.FILE_URI,
-      sourceType : navigator.camera.PictureSourceType.PHOTOLIBRARY,
-      correctOrientation : true,
-      targetWidth : '1024'
-    });
+    // Initialize module
   },
   getPictures : function() {
     
@@ -46,13 +17,10 @@ asi.Picture = {
     $scope.pictures = t.getPictures();
 
     $scope.takePicture = function() {
-      asiLog('$scope.takeAPicture()');
       Event.fire(asi.evt.takePicture);
     };
 
     $scope.share = function(picture) {
-      asiLog('$scope.share: ', picture);
-
       Event.fire(asi.evt.share, {
         message : picture.caption,
         image : picture.imageUrl
