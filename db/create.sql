2040dbb14a5900c26e77412491d6c02a4918f0ab
fgabrieli@gmail.com <fgabrieli@gmail.com>
2015-03-09 16:16:38 -0300

    Adding "deleted" and "abuse" fields. "abuse" is for reported pictures with malicious or improper content.

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,9 @@
-CREATE TABLE `pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fileName` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `caption` varchar(255) DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE pictures (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  fileName varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  caption varchar(255) DEFAULT NULL,
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted tinyint(1) DEFAULT NULL,
+  abuse int(11) DEFAULT '0',
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
