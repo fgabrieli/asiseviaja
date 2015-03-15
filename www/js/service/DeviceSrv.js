45eea37bfe381cd446455198d9cc934c4271bd75
fgabrieli@gmail.com <fgabrieli@gmail.com>
2015-03-15 01:02:47 -0300

    Removed wrappers to touch events in DeviceSrv. Centered pictures for screen 360x640 (galaxy s4 mini) Created logic to know when we are reaching the bottom of the pictures so we ask the backend for more to show.

--- www/js/service/DeviceSrv.js
+++ www/js/service/DeviceSrv.js
@@ -9,31 +9,6 @@ asi.Service.DeviceSrv = $.extend(true, {}, asi.Service, {
  init : function() {
   var t = asi.Service.DeviceSrv;
   t.detect();
-  
-  if (t.device == 'mobile') {
-   t.bindTouchEvents();
-  }
- },
- bindTouchEvents : function() {
-  $('body').bind('touchstart', function() {
-   console.log('touchstart');
-   Event.fire(asi.evt.touchStart, {});
-  });
-
-  $('body').bind('touchmove', function() {
-   console.log('touchmove');
-   Event.fire(asi.evt.touchMove, {});
-  });
-
-  $('body').bind('touchend', function() {
-   console.log('touchend');
-   Event.fire(asi.evt.touchEnd, {});
-  });
-
-  $('body').bind('touchcancel', function() {
-   console.log('touchcancel');
-   Event.fire(asi.evt.touchCancel, {});
-  });
  },
  // public
  getDeviceType : function() {
